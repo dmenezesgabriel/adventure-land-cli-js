3b8dff1dbfbbf6ec3dd1bdf3d3f2dc66a307300a
Gabriel <dmenezes.gabriel@gmail.com>
2021-04-25 21:33:02 -0300

    Add server selection to puppeter

--- services/puppeteer_emulator/main.js
+++ services/puppeteer_emulator/main.js
@@ -2,6 +2,7 @@ import puppeteer from "puppeteer";
 import dotenv from "dotenv";
 import User from "./src/user.js";
 import logger from "./src/logger.js";
+import Game from "./src/game.js";
 
 dotenv.config();
 
@@ -9,12 +10,20 @@ const baseUrl = "https://adventure.land/";
 
 const EMAIL = process.env.EMAIL;
 const PASSWORD = process.env.PASSWORD;
+const TARGET_SERVER_IDENTIFICATOR = process.env.TARGET_SERVER_IDENTIFICATOR;
 const CHARACTERS = process.env.CHARACTERS.split(" ");
 
 let user = new User(EMAIL, PASSWORD);
+
 logger.info("Getting session");
 await user.getSession();
 await user.getCharacters();
+
+logger.info("Getting servers");
+let game = new Game(user.sessionCookie, user.userId);
+await game.getServers();
+
+const targetServer = game.servers[TARGET_SERVER_IDENTIFICATOR];
 
 //  Sleep for x seconds
 async function sleep(seconds) {
@@ -62,6 +71,7 @@ async function main() {
   await page.type("#email2", EMAIL);
   await page.type("#password2", PASSWORD);
   logger.info(`Click login`);
+  await page.screenshot({ path: "/usr/src/app/login.png" });
   await page.evaluate(
     "api_call_l('signup_or_login',{email:$('#email2').val(),password:$('#password2').val(),only_login:true},{disable:$(this)})"
   );
@@ -96,6 +106,13 @@ async function main() {
     const page = await context.newPage();
     await page.goto(baseUrl);
     await sleep(5);
+    await page.evaluate(
+      `server_addr='${targetServer.addr}'; server_port='${targetServer.port}'; init_socket();`
+    );
+    await sleep(5);
+    await page.screenshot({
+      path: "/usr/src/app/before_select_char.png",
+    });
     await page.evaluate(char.loginJS); // select character
     await sleep(5);
     await logPageConsole(page);
