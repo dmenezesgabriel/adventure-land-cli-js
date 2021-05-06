2ac47f48a6c0c9a4583dc9795bdd0a832ca23422
Gabriel <dmenezes.gabriel@gmail.com>
2021-05-06 19:34:54 -0300

    remove dumpio from puppettter

--- services/puppeteer_emulator/src/emulator.js
+++ services/puppeteer_emulator/src/emulator.js
@@ -4,7 +4,7 @@ import { sleep } from "./utils.js";
 
 //  Log page logs
 async function logPageConsole(page) {
-  page.on("console", (msg) => logger.info(`Game log: ${msg.text()}`));
+  page.on("console", (msg) => logger.info(`Page log: ${msg.text()}`));
 }
 
 // Open browser
